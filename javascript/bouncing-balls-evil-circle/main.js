4d107299614f163664e4ea157e596fd05eaab6e9
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-02-18 16:01:12 +0800

    Update main.js

--- javascript/bouncing-balls-evil-circle/main.js
+++ javascript/bouncing-balls-evil-circle/main.js
@@ -152,7 +152,7 @@ class EvilCircle extends Shape {
         if (distance < this.size + balls[j].size) {
           balls[j].exists = false;
           count--;
-          para.innerHTML = '还剩 ' + count + ' 个球'+'<br>不等了,现在就清除<input type=number onblur=\'clearballs(this.value)\'>个球'
+          para.textContent = '还剩 ' + count + ' 个球'+'<br>不等了,现在就清除<input type=number onblur=\'clearballs(this.value)\'>个球'
         }
       }
     }
@@ -214,7 +214,7 @@ function loop() {
       balls[i].onclick=function(){
   this.exists=false
   count=count-num;
-  para.innerHTML = '还剩 ' + count + ' 个球'+'<br>不等了,现在就清除<input type=number onblur=\'clearballs(this.value)\'>个球';
+  para.textContent = '还剩 ' + count + ' 个球'+'<br>不等了,现在就清除<input type=number onblur=\'clearballs(this.value)\'>个球';
       }
     }
   }
@@ -230,6 +230,6 @@ function clearballs(num){
 for(var i=0;i<num;i++){
   balls[i].exists=false
   count=count-num;
-  para.innerHTML = '还剩 ' + count + ' 个球'+'<br>不等了,现在就清除<input type=number onblur=\'clearballs(this.value)\'>个球';
+  para.textContent = '还剩 ' + count + ' 个球'+'<br>不等了,现在就清除<input type=number onblur=\'clearballs(this.value)\'>个球';
  }
 }
