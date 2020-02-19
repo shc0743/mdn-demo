90ab86900dd85bddbf66cbd95805c21e2d5224f4
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-02-19 13:06:10 +0800

    Update main.js

--- javascript/touch-paint/main.js
+++ javascript/touch-paint/main.js
@@ -48,7 +48,7 @@ try{
   el.addEventListener("touchend", handleEnd, false);
   el.addEventListener("touchcancel", handleCancel, false);
   el.addEventListener("touchmove", handleMove, false);
-  canvasize.disabled=0
+  document.getElementById("canvasize").disabled=0
   log("初始化成功。");
     } else {
       throw 'Err in function "zr" '+tmpzr
